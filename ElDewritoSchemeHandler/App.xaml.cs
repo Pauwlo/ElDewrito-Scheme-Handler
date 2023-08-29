0046ad7da94ca1759b0811588dde62c5fa3cc52b
Pauwlo <dev@pauwlo.fr>
2023-08-29 17:30:06 +0200

    Initial commit

---
+++ ElDewritoSchemeHandler/App.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace ElDewritoSchemeHandler
+{
+    public partial class App : Application
+    {
+        public void OnStartup(object sender, StartupEventArgs e)
+        {
+            string query = ParseArgs(e.Args);
+            string eldewritoPath = GetElDewritoPath();
+
+            StartGame(eldewritoPath, query);
+
+            Environment.Exit(0);
+        }
+
+        public string ParseArgs(string[] args)
+        {
+            if (args.Length != 1 || !args[0].StartsWith("eldewrito://"))
+            {
+                string myName = AppDomain.CurrentDomain.FriendlyName;
+                MessageBox.Show($"Usage: {myName} eldewrito://server_ip[:port]");
+                Environment.Exit(1);
+            }
+
+            string arg = args[0].Replace("eldewrito://", "");
+
+            if (arg.EndsWith('/'))
+            {
+                arg = arg.Remove(arg.Length - 1, 1);
+            }
+
+            return arg;
+        }
+
+        public string GetElDewritoPath()
+        {
+            string? eldewritoPath = ConfigurationManager.AppSettings.Get("ElDewritoPath");
+
+            if (File.Exists(eldewritoPath))
+            {
+                return eldewritoPath;
+            }
+
+            MessageBoxResult messageResult = MessageBox.Show(
+                "Please select your ElDewrito instance (eldorado.exe).",
+                "ed where?",
+                MessageBoxButton.OKCancel);
+
+            if (messageResult == MessageBoxResult.Cancel)
+                Environment.Exit(0);
+
+            OpenFileDialog eldoradoDialog = new OpenFileDialog();
+            eldoradoDialog.FileName = "eldorado.exe";
+
+            bool? result = eldoradoDialog.ShowDialog();
+
+            if (result != true) // User cancel
+                Environment.Exit(0);
+
+            eldewritoPath = eldoradoDialog.FileName;
+
+            // Update configuration file
+            Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            configuration.AppSettings.Settings["ElDewritoPath"].Value = eldewritoPath;
+            configuration.Save();
+
+            ConfigurationManager.RefreshSection("appSettings");
+
+            return eldewritoPath;
+        }
+
+        public void StartGame(string path, string query)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = path;
+            p.StartInfo.Arguments = $"-connect {query}";
+            p.StartInfo.UseShellExecute = false;
+
+            p.Start();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# ElDewrito Scheme Handler
+
+This is a small utility that registers a new `eldewrito://` scheme on Windows.
+
+The goal is to run ElDewrito and automatically join a specified server, by clicking on a URI like [eldewrito://allsafe.pauwlo.fr:11720/](eldewrito://allsafe.pauwlo.fr:11720/).
+
+## How it works
+
+Registering a new URI scheme on Windows is pretty straightforward. You just need to add a couple registry keys under `HKEY_CLASSES_ROOT`, like so:
+
+```reg
+Windows Registry Editor Version 5.00
+
+[HKEY_CLASSES_ROOT\eldewrito]
+"URL Protocol"=""
+
+[HKEY_CLASSES_ROOT\eldewrito\shell\open\command]
+@="\"C:\\Path\\To\\Program.exe\" \"%1\""
+```
+
+This way, you tell Windows to run `C:\Path\To\Program.exe` with the specified resource as a command-line argument, when you click on a URI that begins with `eldewrito://...`.
+
+ElDewrito supports joining a server when the game starts with the `-connect` argument. The problem is, it expects a server hostname or IP in the form of `hostname[:port]`, but Windows will call the handler application with the full URI as an argument. This may cause ElDewrito to crash because it doesn't expect to receive the following parameter : `eldorado.exe -connect eldewrito://allsafe.pauwlo.fr:11720/`
+
+To circumvent this, this repository contains a small C# application that not only parses and fixes the server hostname for you, but also registers itself in registry and helps you select your ElDewrito installation path.
+
+This means you can get click-to-join support for ElDewrito in a couple clicks!
